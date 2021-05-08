e83735c2aa639624c3a095b67daf1c808db47300
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2021-05-08 23:02:53 +0800

    add logic for url redirect using gsheets

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+// @ts-ignore
+import PublicGoogleSheetsParser from "public-google-sheets-parser";
 
-createApp(App).mount("#app");
+const spreadsheetId = "1Qdl6oePPqalgQS_XZ8voAkRuHH1bwSXtUhBDANsS7Cs";
+const parser = new PublicGoogleSheetsParser();
+
+parser.parse(spreadsheetId).then((items: any) => {
+  const pathname = window.location.pathname.replace("/", "");
+
+  if (items !== null && items.length > 0) {
+    const urlIndex = items.findIndex((item: any) => {
+      return item.path === pathname;
+    });
+
+    if (urlIndex !== -1) {
+      window.location.replace(items[urlIndex].link);
+      return;
+    }
+  }
+});
