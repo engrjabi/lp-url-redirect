d2820f5db5aa86403e6676ddd5cba307273b3ef4
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2021-05-09 00:43:27 +0800

    updated root path to remove gh root path

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 // @ts-ignore
 import PublicGoogleSheetsParser from "public-google-sheets-parser";
 
+const spreadsheetId = "1Qdl6oePPqalgQS_XZ8voAkRuHH1bwSXtUhBDANsS7Cs";
+/// Same root path here vue.config.js
+const ghPagePath = "lp-url-redirect/";
+
 const path = localStorage.getItem("path");
 console.log("path", path);
 
 if (path) {
   localStorage.removeItem("path");
-
-  const spreadsheetId = "1Qdl6oePPqalgQS_XZ8voAkRuHH1bwSXtUhBDANsS7Cs";
+  const rootPath = path.replace(ghPagePath, "");
   const parser = new PublicGoogleSheetsParser();
 
   parser.parse(spreadsheetId).then((items: any) => {
     if (items !== null && items.length > 0) {
       const urlIndex = items.findIndex((item: any) => {
-        return item.path === path;
+        return item.path === rootPath;
       });
 
       if (urlIndex !== -1) {
