e3ea38c44c6bfc4e4d580c1631a070d1927faf1f
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2021-05-09 00:25:49 +0800

    change master branch name to main

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:engrjabi/lp-url-redirect.git master:gh-pages
+git push -f git@github.com:engrjabi/lp-url-redirect.git main:gh-pages
 cd -
