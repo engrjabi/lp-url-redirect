3d183da5155ae59427d25591cd83804eaf761ec5
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2021-05-09 11:56:58 +0800

    integrated working mansory

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       transition-duration="0.3s"
       item-selector=".item"
       fit-width="true"
-      gutter="10"
+      gutter="3"
     >
       <div
         v-masonry-tile
@@ -14,16 +14,21 @@
         v-for="(item, index) in items"
         :key="index"
       >
+        <img
+          v-if="MansoryItemType.IMAGE === item.type"
+          :src="item.link"
+          class="mansory-image-item"
+        />
+
         <div
+          v-else-if="MansoryItemType.VIDEO === item.type"
           class="fb-video"
-          data-href="https://web.facebook.com/103547664793647/videos/1181762118934339"
+          :data-href="item.link"
           data-allowfullscreen="true"
-          data-width="500"
+          data-width="400"
           data-autoplay="true"
           data-lazy="false"
         />
-
-        <img src="/img/house.jpg" />
       </div>
     </div>
   </div>
@@ -32,11 +37,63 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+enum MansoryItemType {
+  IMAGE = "image",
+  VIDEO = "video",
+}
+
 export default defineComponent({
   name: "App",
   data: () => {
     return {
-      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      MansoryItemType,
+      items: [
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/clouds1.jpg",
+        },
+        {
+          type: MansoryItemType.VIDEO,
+          link:
+            "https://web.facebook.com/103547664793647/videos/1181762118934339?mute=0",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/cup view 2.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/house.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/falls1.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/sunset1.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/clouds2.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/cup view.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/sunset2.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/horizon.jpg",
+        },
+        {
+          type: MansoryItemType.IMAGE,
+          link: "/img/sky.jpg",
+        },
+      ],
     };
   },
   mounted() {
@@ -45,13 +102,13 @@ export default defineComponent({
 
     const finished_rendering = function () {
       console.log("finished rendering plugins");
-      _this.$redrawVueMasonry("containerId");
+      setTimeout(() => _this.$redrawVueMasonry("containerId"), 1000);
     };
 
     // In your onload handler
     (window as any).FB.Event.subscribe("xfbml.render", finished_rendering);
 
-    setTimeout(() => _this.$redrawVueMasonry("containerId"), 300);
+    setTimeout(() => _this.$redrawVueMasonry("containerId"), 500);
   },
 });
 </script>
@@ -68,5 +125,13 @@ export default defineComponent({
 
 .mansory-root {
   margin: 0 auto;
+}
+
+.mansory-image-item {
+  max-width: 400px;
+}
+
+.mansory-root .fb-video {
+  margin-bottom: 3px;
 }
 </style>
