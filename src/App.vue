eba131f8d64729dfef227b4f4b7a833ce008f490
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2021-05-09 12:00:10 +0800

    move asset sources on another file

--- src/App.vue
+++ src/App.vue
@@ -36,64 +36,14 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
-enum MansoryItemType {
-  IMAGE = "image",
-  VIDEO = "video",
-}
+import { assetSources, MansoryItemType } from "@/assetSources";
 
 export default defineComponent({
   name: "App",
   data: () => {
     return {
       MansoryItemType,
-      items: [
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/clouds1.jpg",
-        },
-        {
-          type: MansoryItemType.VIDEO,
-          link:
-            "https://web.facebook.com/103547664793647/videos/1181762118934339?mute=0",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/cup view 2.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/house.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/falls1.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/sunset1.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/clouds2.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/cup view.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/sunset2.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/horizon.jpg",
-        },
-        {
-          type: MansoryItemType.IMAGE,
-          link: "/img/sky.jpg",
-        },
-      ],
+      items: assetSources,
     };
   },
   mounted() {
---
+++ src/assetSources.ts
@@ -0,0 +1,52 @@
+export enum MansoryItemType {
+  IMAGE = "image",
+  VIDEO = "video",
+}
+
+export const assetSources = [
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/clouds1.jpg",
+  },
+  {
+    type: MansoryItemType.VIDEO,
+    link:
+      "https://web.facebook.com/103547664793647/videos/1181762118934339?mute=0",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/cup view 2.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/house.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/falls1.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/sunset1.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/clouds2.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/cup view.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/sunset2.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/horizon.jpg",
+  },
+  {
+    type: MansoryItemType.IMAGE,
+    link: "/img/sky.jpg",
+  },
+];
